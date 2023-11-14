324dad0aaf114519a8a5874c805ed2df37fde8df
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2023-11-14 09:22:28 +0000

    feat: Create functions to process single PDF

--- process_statements.py
+++ process_statements.py
@@ -18,36 +18,35 @@ finyear = "2022-23"
 # Folder where statements are§
 folder_statement = "Factor-statements"
 
-# Single statement
-file = "2022-05.pdf"
-
 # unique reference number
-urn = re.compile("999008")
+rgx_urn = re.compile("999008")
 
 # The start of page 0
-page0_start = re.compile('TOTAL PAYMENTS RECEIVED')
+rgx_page0_start = re.compile('TOTAL PAYMENTS RECEIVED')
 
 # The start of page 1 plus
-page1plus_start =re.compile("Page \d+ of \d+")
+rgx_page1_plus_start =re.compile("Page \d+ of \d+")
 
 # Regex - date (doesn't check for invalid dates)
 re_date = re.compile("(?<=\\n)\d{2}\/\d{2}\/\d{2}")
 
+
+"""
+For testing
+Specify a single file
+"""
+
+# Single statement
+file = "2022-05.pdf"
+
 # Full path (single file)
-fname = os.path.join(parent_path, finyear, folder_statement, file)
+file_path = os.path.join(parent_path, finyear, folder_statement, file)
 
 """ 
 Open up PDF and extract text
 """
 
-# Reader
-reader = PdfReader(fname)
 
-# List of texts
-texts = [page.extract_text() for page in reader.pages]
-
-# Remove blank lines
-texts = [re.sub('\n \n', '\n', text) for text in texts]
 
 """
 To help debug - write out to text file
@@ -73,8 +72,7 @@ def get_next_date(text, re_date):
     startPoint = re_date.search(text).span()[0]
     return text[startPoint:]
 
-
-def processChunk(text, re_date):
+def process_chunk(text, re_date):
 
     """ Returns list containing the following details of each transaction
         Date
@@ -110,79 +108,189 @@ def get_start_next_chunk(text, regex):
     startPoint = regex.search(text).span()[1]+1
     return text[startPoint:]
 
+def get_reader_object(file_path):
 
-""" 
-Page 1 - find the start of the actual statement
-"""
+    """
+        Returns a class 'PyPDF2._reader.PdfReader'
+        
+        Parameters
 
-# Get first page
-page0 = texts[0]
+        file_path (str): Full file path of a single pdf
+    """
 
-# Get start of next chunk
-page0_rem = get_start_next_chunk(page0, page0_start)
+    # Reader
+    reader = PdfReader(file_path)
+    return reader
 
-# Get next date
-page0_rem = get_next_date(page0_rem, re_date)
+def get_text_from_pages(reader):
 
-chunk = processChunk(page0_rem, re_date)
-chunk
+    """
+        Returns a list of strings 
+        Length of list matches number of pages in pdf
 
-"""
-Page 1 - get next chunk
-Repeat this until......
-"""
-chunks = [chunk]
+        Parameters
 
-while chunk is not None:
+        reader (class 'PyPDF2._reader.PdfReader')
 
-    # First page - remainder
-    page0_rem = get_start_next_chunk(page0_rem, urn)
-    #page0_rem = get_next_date(page0_rem, re_date)
+    """
 
-    # Get chunk
-    chunk = processChunk(page0_rem, re_date)
-    if chunk is not None: chunks.append(chunk) 
+    # List of pages (text content)
+    pages = [page.extract_text() for page in reader.pages]
 
-print(len(chunks))
+    # Remove blank lines
+    pages = [re.sub('\n \n', '\n', text) for text in pages]
 
-""" 
-Page 2+ 
-Get to first bit that says "page x of y"
-"""
+    return pages
 
-# For remainder of pages
-for page in texts[1:]:
+def get_next_entry(page_rem, rgx_page_start, rgx_date):
+
+    """
+        Returns string, with remainder of page
+        
+        Parameters
+
+        page_rem (str): String containing remainder (rem) of page
+        rgx_page_start (compiled regex)
+        rgx_date
+
+    """
+
+    # Get start of next chunk
+    page_rem = get_start_next_chunk(page_rem, rgx_page_start)
+
+    # Get next date
+    page_rem = get_next_date(page_rem, rgx_date)
+
+    return page_rem
+
+def process_page_0(page_rem, re_date, rgx_urn):
+
+    """
+
+        Parameters
+
+        page_rem (str)
+        chunks (list)
+        re_date
+        rgx_urn
+
+    """
+
+    # Init as not None to start process
+    chunk = ''
+
+    chunks = []
+
+    while chunk is not None:
+
+        # Page - remainder
+        page_rem = get_start_next_chunk(page_rem, rgx_urn)
+        #page0_rem = get_next_date(page0_rem, re_date)
+
+        # Get chunk
+        chunk = process_chunk(page_rem, re_date)
+        if chunk is not None: chunks.append(chunk) 
+    return page_rem, chunks
+
+def process_all_page_0(page_rem, rgx_page0_start, re_date):
+
+    """
+        Process all of the first page of a PDF
+    """
+
+    # Get start page of page
+    page_rem = get_next_entry(page_rem, rgx_page0_start, re_date)
+
+    # Get first chunk
+    chunks = [process_chunk(page_rem, re_date)]
+
+    # Process remainder of page
+    page_rem, new_chunks = process_page_0(page_rem, re_date, rgx_urn)
+
+    # Add new chunks
+    chunks.extend(new_chunks)
+    print(f"Chunks length = {len(chunks)}")
+
+    return chunks
+
+def process_all_other_pages(page, rgx_page1_plus_start, rgx_urn):
+
+    """
+        Processes pages 1+
+    """
+
+    # init empty list
+    chunks = []
 
     # Copy page to remainder object
     page_rem = page
 
-    # Init firstPage (boolean) and chunk as not None
-    firstPage = True
+    # Init page_start (boolean) and chunk as not None
+    page_start = True
     chunk = ''
 
     while chunk is not None:
 
-        # If first page then we use the page x of y logic
-        if firstPage:
-            page_rem = get_start_next_chunk(page_rem, page1plus_start)
-            firstPage = False # Set to False for remainder of page
+        # If start of page then we use the page x of y logic
+        if page_start:
+            page_rem = get_start_next_chunk(page_rem, rgx_page1_plus_start)
+            page_start = False # Set to False for remainder of page
         else:
 
             # For other parts of page we need to use URN
-            page_rem = get_start_next_chunk(page_rem, urn)
+            page_rem = get_start_next_chunk(page_rem, rgx_urn)
 
         # Process chunk
-        chunk = processChunk(page_rem, re_date)
+        chunk = process_chunk(page_rem, re_date)
 
         # Add to list if there's data
         if chunk is not None: chunks.append(chunk)
 
-print(len(chunks))
+    return chunks
 
-"""
-With chunks - create dataframe
-"""
-columns = ["date", "description_1", "description_2", "txn_amount", "txn_share", "txn_actual"]
-df = pd.DataFrame(data=chunks, columns=columns)
+def get_dataframe(chunks):
 
-df.to_csv('blah.csv')
+    """
+
+    """
+    # Column names
+    columns = ["date", "description_1", "description_2", "txn_amount", "txn_share", "txn_actual"]
+    df = pd.DataFrame(data=chunks, columns=columns)
+    return df
+
+def process_single_pdf(file_path):
+
+    """
+
+        Parameters:
+
+        file_path (str): Full file path of a single pdf
+
+    """
+
+    # Get reader object
+    reader = get_reader_object(file_path)
+
+    # Pages (text content)
+    pages = get_text_from_pages(reader)
+
+    # Process each page in single pdf
+    for page_num in range(len(pages)):
+
+        print(f"Processing page number {page_num}")
+
+        # Current page
+        page = pages[page_num]
+
+        if page_num == 0:
+
+            # Process all of page 0
+            chunks = process_all_page_0(page, rgx_page0_start, re_date)
+
+        else:
+
+            # Get new chunks
+            new_chunks = process_all_other_pages(page, rgx_page1_plus_start, rgx_urn)
+            chunks.extend(new_chunks)
+
+    return chunks
